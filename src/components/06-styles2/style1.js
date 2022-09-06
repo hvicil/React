0002be966265e8e7abdfcf578f987e9e51d07dc5
hvicil <100441731+hvicil@users.noreply.github.com>
2022-09-06 23:25:44 +0200

    React2

---
+++ src/components/06-styles2/style1.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Style1 = () => {
+  return (
+    <div
+      style={{
+        border: "1px solid red",
+        padding: "1rem",
+        borderRadius: "1rem",
+        textAlign: "center",
+      }}
+    >
+      <h1
+        style={{
+          margin: 0,
+          color: "navy",
+        }}
+      >
+        React Fundementals
+      </h1>
+      <p
+        style={{
+          fontStyle: "italic",
+        }}
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur,
+        minus. Molestias impedit aperiam consequuntur explicabo illo, cupiditate
+        assumenda, porro odio dolorem architecto dignissimos? Officia, hic
+        assumenda cumque quis consectetur voluptates?
+      </p>
+    </div>
+  );
+};
+
+export default Style1;
